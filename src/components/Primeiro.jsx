51557998e2166bf8888f3d1509f2959c07a2d10c
Ebenezer Cavalcante <ebenezercavalcante@iMac-de-Gabriel.local>
2021-05-26 21:18:53 -0300

    initial commit

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Primeiro(){
+    return (
+        <div>
+            <h1>Primeiro Compornente</h1>
+            <h2>Exemplo de um Componente React</h2>
+        </div>
+    )
+}
+
+export default Primeiro
